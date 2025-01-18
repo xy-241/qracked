d5f8f96bd5cbad458a463f4406a3a32fed475f73
Daren Tan <daren28@gmail.com>
2025-01-18 16:17:15 +0800

    working qr-code replace for kahoot

--- contents/all-urls.ts
+++ contents/all-urls.ts
@@ -1,21 +1,40 @@
 import type { PlasmoCSConfig } from "plasmo"
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://www.hiokkuek.info/"]
+  // all urls 
+  matches: ["<all_urls>"],
 }
 
-window.addEventListener("load", () => {
+// interval to search every 2 seconds
+setInterval( () => {
   const printPageHTML = () => {
     const pageHTML = document.documentElement.outerHTML
-    console.log(pageHTML)
+    // console.log(pageHTML)
   }
   printPageHTML()
 
   const removeCanvasElements = () => {
-    const canvasElements = document.querySelectorAll("img")
+    // #react-qrcode-logo is the id for the canvas element of kahoot qr code
+    const canvasElements = document.querySelectorAll("#react-qrcode-logo");
+
+    // replace canvas with new image element
     canvasElements.forEach((canvas) => {
-      canvas.remove()
+      const newImg = document.createElement("img");
+      
+      // new image is nus hackers logo
+      newImg.src = "https://media.licdn.com/dms/image/v2/D560BAQE2pJ2bMhfGhQ/company-logo_200_200/company-logo_200_200/0/1691820001578/nus_hackers_logo?e=2147483647&v=beta&t=jVYrYmhSDz_oyTejx5WlPVtZIRdbaVZaGnFcv_iJ7fE";
+      newImg.width = canvas.clientWidth;
+      newImg.height = canvas.clientHeight;
+
+      canvas.parentNode.replaceChild(newImg, canvas);
     })
+
+
+    // for remove elements
+    // canvasElements.forEach((canvas) => {
+    //   canvas.remove()
+    // })
+    // kahootQR.remove();
   }
   removeCanvasElements()
-})
+}, 2000);
